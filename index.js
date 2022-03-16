5feb104e8b015f328aba7a0b70fd9a457ea0764f
John-gith <100775197+John-gith@users.noreply.github.com>
2022-03-16 16:21:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var config = require('./configTwit');
 var T = new Twit(config);
 var CronJob = require('cron').CronJob;
 
-const myAccount = '1500224900463742979'; //Ur account to avoid replying to yourself
-var statusTweet = `I have noticed a new banking protocol on the solana blockchain. It's a very interesting project with a lot of potential to be the next successor in the DeFi realm. They also merge with web3. Check @trush_io`; //Add your tweet here
+const myAccount = '1501686863584768000'; //Ur account to avoid replying to yourself
+var statusTweet = `Visit the future of Finance in crypto: @trush_io. Their services are the solution for utilising the daily usage of financial services! `; //Add your tweet here
 var isRunning = false;
-var hashtags = ['#defi', '#solana', '#airdrop' ,'#crypto' ,'#web3' , '#metaverse'];
+var hashtags = ['#airdrop'];
 
-var job = new CronJob('*/10 * * * *', function () {
+var job = new CronJob('*/15 * * * *', function () {
     if (isRunning == false) {
         makeTweet();
         isRunning = true;
